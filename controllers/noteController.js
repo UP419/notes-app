90266c5f3fcec71b06601412b247eb3949a3cc2b
ucha <uchaucha419@gmail.com>
2024-05-27 01:35:28 +0400

    small changes in note controller and page

--- controllers/noteController.js
+++ controllers/noteController.js
@@ -28,23 +28,39 @@ module.exports.addNote = async (req, res) => {
 module.exports.getAllNotes = async (req, res) => {
     try {
         const notes = await Note.find();
+        console.log(notes);
         res.status(200).json(notes);
+        
       } catch (err) {
         res.status(400).json({ error: err.message });
       }
 }
 
 module.exports.getNotesByUser = async (req, res) => {
-    const { user } = req.body;
-    try {
-      const notes = await Note.find({ user });
-      if (!notes.length) {
-        return res.status(404).json({ error: 'No notes found for this user' });
-      }
-      res.status(200).json(notes);
-    } catch (err) {
-      res.status(400).json({ error: err.message });
-    }
+    const token = req.cookies.jwt;
+    console.log('');
+    if (token) {
+        jwt.verify(token, 'this is jwt secred string', async (err, decodedToken) => {
+          if (err) {
+            res.locals.user = null;
+            next();
+          } else {
+            let user = await User.findById(decodedToken.id);
+            try {
+                console.log(user.email);
+                const notes = await Note.find({ user: user.email });
+                console.log('notes found');
+                console.log(notes);
+                if (!notes.length) {
+                  return res.status(404).json({ error: 'No notes found for this user' });
+                }
+                res.status(200).json(notes);
+            } catch (err) {
+                res.status(400).json({ error: err.message });
+            }       
+          }
+        });
+    }    
 }
 
 module.exports.deleteNote = async (req, res) => {
